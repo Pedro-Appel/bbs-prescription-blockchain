7ae02786b2fbe520b05e0f72b173139bfc5a219d
Pedro Appel <appelpedroappel@gmail.com>
2023-09-12 21:53:46 -0300

    fix: removing useless imports

--- src/main/java/br/com/bbs/blockchain/service/ChainRepository.java
+++ src/main/java/br/com/bbs/blockchain/service/ChainRepository.java
@@ -3,7 +3,6 @@ package br.com.bbs.blockchain.service;
 import br.com.bbs.blockchain.model.Block;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ChainRepository {
     void save(Block entity);
